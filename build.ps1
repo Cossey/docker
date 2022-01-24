ae6f984c9ff5ac2543c3e5c6828d118f80b66c3f
Stewart Cossey <stewart.cossey@gmail.com>
2022-01-24 15:22:38 +1300

    Update unified build tool.

--- build.ps1
+++ build.ps1
@@ -16,12 +16,12 @@ Switch ($output.ToLower()) {
     "load" {break}
     "folder" {
         if (!$outputpath) {
-            Write-Error "Required parameter outputpath is missing"
+            Write-Host "Required parameter -outputpath is missing" -ForegroundColor Red
             exit 2
         }
     }
     default {
-        Write-Error "Invalid output type"
+        Write-Host "Invalid output type" -ForegroundColor Red
         exit 1
     }
 }
@@ -46,11 +46,11 @@ function ParseVersion() {
     if ($version) {
         $script:tagver = $version
     } else {
-        if ($VERSIONFILE && $VERSIONREGEX) {
+        if ($VERSIONFILE -and $VERSIONREGEX) {
             $vf = Join-Path -Path $image -ChildPath $VERSIONFILE
             $script:tagver = (Get-Content $vf | Select-String -Pattern $VERSIONREGEX).Matches[0].Groups[1].Value
         } else {
-            Write-Error "The version parameter is required for this image"
+            Write-Host "The version parameter is required for this image" -ForegroundColor Red
             exit 3
         }
     }
@@ -63,23 +63,30 @@ function LoadEnvVarFile($path) {
         } elseif ($_ -match '^\s*$') {
             # skip empty lines
         } else {
-            $parts = $_.Split('=')
+            $parts = $_.Split('=', 2)
             $name = $parts[0]
             $value = $parts[1]
-            Set-Variable -Name $name -Value $value -Scope global
+            Set-Variable -Name $name -Value $value -Scope script
         }
+    }
+}
+
+function DownloadFiles() {
+    if ($DOWNLOADFILE) {
+        Invoke-WebRequest -Uri $DOWNLOADFILE -OutFile (Join-Path $image $(Split-Path -Path $DOWNLOADFILE -Leaf))
     }
 }
 
 if (Test-Path "./$image") {
     if (Test-Path "./$image/build.env") {
         LoadEnvVarFile "./$image/build.env"
+        DownloadFiles
         ParseVersion
         BuildX
     } else {
-        Write-Error "The image does not support the unified build tool"
+        Write-Host "The image does not support the unified build tool" -ForegroundColor Red
         exit 4
     }
 } else {
-    Write-Error "Folder not found: $image"
+    Write-Host "Folder not found: $image" -ForegroundColor Red
 }

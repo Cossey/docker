803969d88e641fd8c9fa73d4d601a11f4a8d5291
Stewart Cossey <stewart.cossey@gmail.com>
2023-07-02 22:42:49 +1200

    Remove build scripts for updater and backup.

--- filebackup/build.ps1
+++
@@ -1,4 +0,0 @@
-$ver = $args[0]
-$tag = "${env:DOCKER_HUB_PROFILE}/filebackup"
-
-docker buildx build -t ${tag}:${ver} -t ${tag}:latest --platform linux/arm/v6,linux/arm/v7,linux/arm64,linux/386,linux/amd64,linux/s390x . --push
